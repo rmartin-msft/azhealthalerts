11b9aea70f466e9d1231ca31614e957ecfe71265
Pipeline Agent <dummy@dummy.com>
2023-05-25 17:40:25 +0100

    initial setup

---
+++ CreateMaintainanceAlert.ps1
@@ -0,0 +1,23 @@
+# Requires Az Powershell - Use Install-Module Az to install latest version
+
+$allSubs = Get-AzSubscription
+
+foreach ($sub in $allSubs) {
+    Write-Host "👉 Setting Context to subscrption $sub.Id"
+
+    Select-AzSubscription -Subscription $sub.Id    
+    New-AzResourceGroup -Name 'global-alerts' -Location 'Global'
+
+    $alertGroup = (New-AzResourceGroupDeployment -Name 'DefaultEmailActionGroup' -ResourceGroupName 'global-alerts' -TemplateFile ./AlertActionGroup.json -actionGroupName 'Planned Maintainance v1' -actionGroupShortName 'emailPlanned')
+    $alertRule = (New-AzResourceGroupDeployment -Name 'DefaultEmailActionGroupRule' -ResourceGroupName 'global-alerts' -TemplateFile ./maintainanceAlert.json -actionGroupResourceId $alertGroup.Outputs.actionGroupId.Value) -logAlertName 'Email_Maintainance'
+}
+
+
+
+
+
+
+
+
+
+
